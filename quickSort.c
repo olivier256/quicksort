c4c5a967fc1173a9544a2c9e560134b38a3049b6
Olivier <olivier.brosseron@icloud.com>
2021-12-18 11:55:06 +0100

    bsr plutôt que jsr (ax). 4080 cycles / -1%

--- quickSort.c
+++ quickSort.c
@@ -11,7 +11,6 @@
 
 void dispArray(int *, int);
 void quickSort(int *, int, int);
-void quickSortTailCall(int *, int, int);
 int partition(int *, int, int, int);
 void swap(int *, int, int);
 int equalsArray(int *, int *, int);
@@ -22,7 +21,7 @@ int main() {
 	printf("Tests OK\n");
 	int t[N] = {4, 6, 3, 2, 1, 9, 7};
 	dispArray(t, N);
-	quickSortTailCall(t, 0, N-1);
+	quickSort(t, 0, N-1);
 	dispArray(t, N);
 }
 
@@ -36,20 +35,10 @@ void dispArray(int *t, int len) {
 
 void quickSort(int *t, int left, int right) {
 	if(right > left) {
-		int pivot = t[right];
-		int partitionPoint = partition(t, left, right, pivot);
-		quickSort(t, left, partitionPoint - 1);
-		quickSort(t, partitionPoint + 1, right);
-	}		  
-}
-
-void quickSortTailCall(int *t, int left, int right) {
-	if(right > left) {
 		while (right > left) {
 			int pivot = t[right];
 			int partitionPoint = partition(t, left, right, pivot);
 			quickSort(t, left, partitionPoint - 1);
-//			quickSort(t, partitionPoint + 1, right);
 			left = partitionPoint + 1;
 		}
 	}		  
@@ -99,43 +88,43 @@ void testQuickSort() {
 	int len = 1;
 	int T1[1] = {4};
 	int V1[1] = {4};
-	quickSortTailCall(T1, 0, len - 1);
+	quickSort(T1, 0, len - 1);
 	assert(equalsArray(T1, V1, len));
 	
 	len = 2;
 	int T2[2] = {4, 6};
 	int V2[2] = {4, 6};
-	quickSortTailCall(T2, 0, len - 1);
+	quickSort(T2, 0, len - 1);
 	assert(equalsArray(T2, V2, len));
 	
 	len = 3;
 	int T3[3] = {4, 6, 3};
 	int V3[3] = {3, 4, 6};
-	quickSortTailCall(T3, 0, len - 1);
+	quickSort(T3, 0, len - 1);
 	assert(equalsArray(T3, V3, len));
 	
 	len = 4;
 	int T4[4] = {4, 6, 3, 2};
 	int V4[4] = {2, 3, 4, 6};
-	quickSortTailCall(T4, 0, len - 1);
+	quickSort(T4, 0, len - 1);
 	assert(equalsArray(T4, V4, len));
 	
 	len = 5;
 	int T5[5] = {4, 6, 3, 2, 1};
 	int V5[5] = {1, 2, 3, 4, 6};
-	quickSortTailCall(T5, 0, len - 1);
+	quickSort(T5, 0, len - 1);
 	assert(equalsArray(T5, V5, len));
 	
 	len = 6;
 	int T6[6] = {4, 6, 3, 2, 1, 9};
 	int V6[6] = {1, 2, 3, 4, 6, 9};
-	quickSortTailCall(T6, 0, len - 1);
+	quickSort(T6, 0, len - 1);
 	assert(equalsArray(T6, V6, len));
 	
 	len = 7;
 	int T7[7] = {4, 6, 3, 2, 1, 9, 7};
 	int V7[7] = {1, 2, 3, 4, 6, 7, 9};
-	quickSortTailCall(T7, 0, len - 1);
+	quickSort(T7, 0, len - 1);
 	assert(equalsArray(T7, V7, len));
 
 }
